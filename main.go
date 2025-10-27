42799f3247259777b86b49b658a7b59c311633e4
smarjane <marjane.selman@ynov.com>
2025-10-27 15:23:25 +0100

    exercice3fonctionnalité

--- main.go
+++ main.go
@@ -14,9 +14,11 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
-	
+
 	http.HandleFunc("/url", URLHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//exercice 3 : 1er fonctionnalité
