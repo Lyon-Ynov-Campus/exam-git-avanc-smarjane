f175c0efa24e7cbb8d423aebc290b108081e5a46
smarjane <marjane.selman@ynov.com>
2025-10-27 16:09:29 +0100

    ex4etape1

--- main.go
+++ main.go
@@ -31,3 +31,14 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 >>>>>>> 0168ec7 (Initialize dev)
+...
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+...
+
+func main() {
+...
+	http.HandleFunc("/color", ColorHandler)
+...
+}
